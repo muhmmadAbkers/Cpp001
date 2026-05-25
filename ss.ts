c043ba9681a9fedb9963787fcae9f5509da0b3ba
muhmmadAbkers <sootoomnb654gmail.com>
2026-05-25 16:09:51 +0300

    git add ss.ts

--- ss.ts
+++ ss.ts
@@ -1,2 +1,8 @@
 let q2 = "SELECT * FROM users WHERE id = 1";
 console.log(q2);
+
+
+var q3 = "SELECT * FROM users WHERE id = 1";
+console.log(q3);
+const q4 = "SELECT * FROM users WHERE id = 1";
+console.log(q4);    
