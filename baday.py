67c546f9b96d776c18df7cd4d6b03190a13c037c
muhmmadAbkers <sootoomnb654gmail.com>
2026-05-31 13:05:35 +0300

    how to use map functinos has alot of setteings

---
+++ baday.py
@@ -0,0 +1,8 @@
+print("hello me please");
+input("enter your name: ")
+print("hello " + input("enter your name: "))
+print("hello " + input("enter your name: "))
+print("hello " + input("enter your name: "))                    
+
+population = 1000000'
+print("the population is " + population)    ;
