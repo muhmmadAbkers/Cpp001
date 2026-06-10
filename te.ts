d63e6b1e83db11c3cf11fe96ba67a0f9e13cc91f
muhmmadAbkers <sootoomnb654gmail.com>
2026-06-10 12:30:41 +0300

    remove it now

--- te.ts
+++
@@ -1,7 +0,0 @@
-const app = new Map()
-
-app.set('name', 'MyApp')
-app.set('version', '1.0.0')     
-app.set('author', 'John Doe')
-app.set('features', ['feature1', 'feature2', 'feature3'])
-console.log(app.get(2));
